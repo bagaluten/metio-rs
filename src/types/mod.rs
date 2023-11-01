11e72d7960a616829013278bb4585d4029be07e0
Kevin Stiehl <kevin.stiehl@numericas.de>
2023-11-01 17:26:48 +0100

    move everything to types package and reexport
    this might be a good idea. Or not we will see

---
+++ src/types/mod.rs
@@ -0,0 +1,24 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct EventType {
+    pub package: String,
+    pub group: String,
+    pub name: String,
+    pub version: String,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct Event {
+    #[cfg_attr(feature = "serde", serde(rename = "eventId"))]
+    pub event_id: String,
+
+    #[cfg_attr(feature = "serde", serde(rename = "objectId"))]
+    pub object_id: String,
+
+    #[cfg_attr(feature = "serde", serde(rename = "eventType"))]
+    pub event_type: EventType,
+}
