956ae878f184b0dd65cad7788bf43277fb589ec9
Kevin Stiehl <kevin.stiehl@numericas.de>
2023-11-04 09:47:58 +0100

    add generated tests & let ci build all features

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -24,3 +24,72 @@ pub struct Event {
 
     pub timestamp: chrono::DateTime<chrono::Utc>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_event_type() {
+        let event_type = EventType {
+            package: "package".to_string(),
+            group: "group".to_string(),
+            name: "name".to_string(),
+            version: "version".to_string(),
+        };
+
+        assert_eq!(event_type.package, "package");
+        assert_eq!(event_type.group, "group");
+        assert_eq!(event_type.name, "name");
+        assert_eq!(event_type.version, "version");
+    }
+
+    #[test]
+    fn test_event() {
+        let event = Event {
+            event_id: "event_id".to_string(),
+            object_id: "object_id".to_string(),
+            event_type: EventType {
+                package: "package".to_string(),
+                group: "group".to_string(),
+                name: "name".to_string(),
+                version: "version".to_string(),
+            },
+            timestamp: chrono::Utc::now(),
+        };
+
+        assert_eq!(event.event_id, "event_id");
+        assert_eq!(event.object_id, "object_id");
+        assert_eq!(event.event_type.package, "package");
+        assert_eq!(event.event_type.group, "group");
+        assert_eq!(event.event_type.name, "name");
+        assert_eq!(event.event_type.version, "version");
+    }
+    
+    #[test]
+    #[cfg(feature = "serde")]
+    fn test_serialization() {
+        let event = Event {
+            event_id: "event_id".to_string(),
+            object_id: "object_id".to_string(),
+            event_type: EventType {
+                package: "package".to_string(),
+                group: "group".to_string(),
+                name: "name".to_string(),
+                version: "version".to_string(),
+            },
+            timestamp: chrono::Utc::now(),
+        };
+
+        let serialized = serde_json::to_string(&event).unwrap();
+        println!("{}", serialized);
+        let deserialized: Event = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(event.event_id, deserialized.event_id);
+        assert_eq!(event.object_id, deserialized.object_id);
+        assert_eq!(event.event_type.package, deserialized.event_type.package);
+        assert_eq!(event.event_type.group, deserialized.event_type.group);
+        assert_eq!(event.event_type.name, deserialized.event_type.name);
+        assert_eq!(event.event_type.version, deserialized.event_type.version);
+    }
+}
