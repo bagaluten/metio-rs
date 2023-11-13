ed85596fae5a1049d19f9aec8f780245561ab07b
Kevin Stiehl <kevin.stiehl@numericas.de>
2023-11-13 20:49:03 +0100

    add parse function to type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -4,10 +4,28 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct EventType {
-    pub package: String,
     pub group: String,
     pub name: String,
     pub version: String,
+}
+
+impl std::str::FromStr for EventType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split('/')
+            .collect::<Vec<&str>>();
+
+        if parts.len() != 3 {
+            return Err(format!("invalid event type: {}", s));
+        }
+
+        Ok(Self {
+            group: parts[0].to_string(),
+            name: parts[1].to_string(),
+            version: parts[2].to_string(),
+        })
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -32,13 +50,11 @@ mod tests {
     #[test]
     fn test_event_type() {
         let event_type = EventType {
-            package: "package".to_string(),
             group: "group".to_string(),
             name: "name".to_string(),
             version: "version".to_string(),
         };
 
-        assert_eq!(event_type.package, "package");
         assert_eq!(event_type.group, "group");
         assert_eq!(event_type.name, "name");
         assert_eq!(event_type.version, "version");
@@ -50,7 +66,6 @@ mod tests {
             event_id: "event_id".to_string(),
             object_id: "object_id".to_string(),
             event_type: EventType {
-                package: "package".to_string(),
                 group: "group".to_string(),
                 name: "name".to_string(),
                 version: "version".to_string(),
@@ -60,7 +75,6 @@ mod tests {
 
         assert_eq!(event.event_id, "event_id");
         assert_eq!(event.object_id, "object_id");
-        assert_eq!(event.event_type.package, "package");
         assert_eq!(event.event_type.group, "group");
         assert_eq!(event.event_type.name, "name");
         assert_eq!(event.event_type.version, "version");
@@ -73,7 +87,6 @@ mod tests {
             event_id: "event_id".to_string(),
             object_id: "object_id".to_string(),
             event_type: EventType {
-                package: "package".to_string(),
                 group: "group".to_string(),
                 name: "name".to_string(),
                 version: "version".to_string(),
@@ -87,7 +100,6 @@ mod tests {
 
         assert_eq!(event.event_id, deserialized.event_id);
         assert_eq!(event.object_id, deserialized.object_id);
-        assert_eq!(event.event_type.package, deserialized.event_type.package);
         assert_eq!(event.event_type.group, deserialized.event_type.group);
         assert_eq!(event.event_type.name, deserialized.event_type.name);
         assert_eq!(event.event_type.version, deserialized.event_type.version);
